ef2f8dbd43747c1c820db98af9f0e28ffb551548
Carlos Alfredo Pinto Hernández <70851771+carlosmding@users.noreply.github.com>
2023-01-20 22:12:24 +0000

    Método stuart_game en construcción

--- main.py
+++ main.py
@@ -11,11 +11,34 @@
 # Stuart has to make words starting with consonants.
 def stuart_game(word):
   iterator = 1
+  findings = []
+  position_initial = 0
+  position_final = 1
+  
   while iterator <= len(word):
-    print("")
+
+    while position_final<=len(word):
+      sub_string = word[position_initial:position_final]
+      if not sub_string[0] in "AEIOU":
+        dict_find = {sub_string : word.count(sub_string)}
+        print(dict_find)
+        findings.append(dict_find)
+        position_initial +=1
+        position_final += 1
+      else:
+        position_initial +=1
+        position_final += 1
+
+    iterator +=1
+    position_initial = 0
+    position_final = iterator
+    
+      
 
     
 # Kevin has to make words starting with vowels.
 def kevin_game(word):
   iterator = 1
-  
+
+if __name__ == "__main__":
+  stuart_game("BANANA")
