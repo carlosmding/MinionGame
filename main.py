5a35795d12c6c89389bf2d080fc8e37a22f48806
Carlos Alfredo Pinto Hernández <70851771+carlosmding@users.noreply.github.com>
2023-01-20 21:33:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+###Kevin and Stuart want to play the 'The Minion Game'.
+
+#Game Rules
+#Both players are given the same string, both players have to make substrings using the letters of the string .
+#The game ends when both players have made all possible substrings. A player gets +1 point for each occurrence of the substring in the string.
+
+
+
+
+
+# Stuart has to make words starting with consonants.
+def stuart_game(word):
+  iterator = 1
+  while iterator <= len(word):
+    print("")
+
+    
+# Kevin has to make words starting with vowels.
+def kevin_game(word):
+  iterator = 1
+  
